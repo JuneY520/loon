27031bc9d559544412cdaae18b274509c278348d
Luv <124025217+JuneY520@users.noreply.github.com>
2024-11-04 20:18:43 +0800

    更新 yaliyali.js

--- yaliyali.js
+++ yaliyali.js
@@ -1,11 +1,12 @@
 // 读取原始响应内容
 let obj = JSON.parse($response.body);
 
-// 只保留导航栏显示配置
+// 只保留导航栏显示配置和历史记录配置
 obj.data = {
   find_config: {
     bottom_nav_name: obj.data.find_config.bottom_nav_name // 保留底部导航栏名称
-  }
+  },
+  history_config: obj.data.history_config // 保留历史记录配置
 };
 
 // 返回修改后的响应内容
