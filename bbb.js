30ea382c07a71d9d2ec752cdad7b0c6e2e9068a2
Luv <124025217+JuneY520@users.noreply.github.com>
2025-04-29 13:23:14 +0800

    更新 bbb.js

--- bbb.js
+++ bbb.js
@@ -1,8 +1,7 @@
 [Script]
 http-response ^https?:\/\/appi\.lanerc\.net\/app\/rank requires-body=1 max-size=0,script-path=https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/bbb.js,tag=屏蔽广告-APP-RANK
 
-[MITM]
-hostname = appi.lanerc.net
+
 
 // empty-rank.js
 var body = JSON.stringify({
@@ -11,3 +10,6 @@ var body = JSON.stringify({
   data: []
 });
 $done({ body });
+
+[MITM]
+hostname = appi.lanerc.net
