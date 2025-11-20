24e3fb74ed285e9d02dfb01a89b549303f515aa9
Luv <124025217+JuneY520@users.noreply.github.com>
2025-11-21 00:09:50 +0800

    Update juziyinshi.js

--- juziyinshi.js
+++ juziyinshi.js
@@ -1,4 +1,22 @@
+[Rewrite]
+# 用户信息接口（包含开屏广告/弹窗广告）
+^http:\/\/110\.42\.3\.216:8088\/api\/user\/uinfo$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
 
+# 首页接口广告
+^http:\/\/110\.42\.3\.216:8088\/api\/home\/.*$ script-response-body  https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
+
+# 配置（包含开屏广告/活动）
+^http:\/\/110\.42\.3\.216:8088\/api\/config\/.*$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
+
+# 广告接口
+^http:\/\/110\.42\.3\.216:8088\/api\/ads\/.*$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
+
+# 启动页广告
+^http:\/\/110\.42\.3\.216:8088\/api\/open\/.*$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
+^http:\/\/110\.42\.3\.216:8088\/api\/start\/.*$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
+
+# 初始化接口
+^http:\/\/110\.42\.3\.216:8088\/api\/init\/.*$ script-response-body https://raw.githubusercontent.com/JuneY520/loon/refs/heads/main/juziyinshi.js
 /*
 桶汁影视 全套增强去广告（MITM版）
 移除：开屏广告、弹窗广告、banner、活动、推荐广告等
